047640a4f1c4e9b4de4ec082f757b13cdc7ed2f8
Alex <alexportelli12@gmail.com>
2023-08-19 11:20:36 +0200

    updates to package.json

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.less']
+  styleUrls: ['./app.component.less'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'personal-finance-tool';
+
+  // yearlyExpensesForm;
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit() {}
 }
